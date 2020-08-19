48ff8626b247c6480d7308ce33db003618a84bf4
Quci <xujiejiuxi@gmail.com>
2020-08-19 22:40:52 +0800

    2.3.3-EventEmitter-demo-2-6

---
+++ 2.3.3-EventEmitter-demo-2-6.js
@@ -0,0 +1,11 @@
+let eventEmitter = require('events').EventEmitter;
+let counter = 0;
+let em = new eventEmitter();
+
+setInterval(() => {
+    em.emit('timed', counter++);
+}, 3000)
+
+em.on('timed', (data) => {
+    console.log(`timed${data}`)
+})
