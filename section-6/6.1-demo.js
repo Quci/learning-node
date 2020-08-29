8c655726577da63db940367317a62a0f28e9d950
Quci <xujiejiuxi@gmail.com>
2020-08-29 17:52:42 +0800

    6.1-demo: "os" module

---
+++ section-6/6.1-demo.js
@@ -0,0 +1,12 @@
+let os = require('os');
+
+console.log(`Using end of line${os.EOL}to insert a new line`);
+console.log(os.endianness());
+console.log(os.tmpdir());
+console.log(os.homedir());
+
+console.log(os.freemem());
+console.log(os.loadavg());
+console.log(os.totalmem());
+console.log(os.cpus());
+
